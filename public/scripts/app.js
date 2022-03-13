c3443b73b9ec4b3117ccefd94d03324ad3e3f2ce
Kent Gulka <kentgulka@modindsoft.com>
2022-03-13 23:23:17 +0000

    created client side logic to display map name/desc in the maps_list div

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,100 @@
 // Client facing scripts here
+
+
+$(document).ready(function() {
+  //this escapes the content
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  //this creates the HTML for a single map item
+  const createMapElement = function(mapData) {
+
+    const $map = $(`
+      <p class="name">${escape(mapData.name)}</p>
+      <p>${escape(mapData.description)}</p>
+    `);
+    return $map;
+  };
+  //this appends map items to the maps_list element in index.ejs.
+  const renderMaps = function (arrMapData) {
+    for (let mapData of arrMapData.maps) {
+      const $map = createMapElement(mapData);
+      $('.maps_list').append($map);
+    }
+    return;
+  }
+  //this request the data from /maps GET route and renders all map items
+  const loadMaps = function() {
+    $.ajax('/maps', { method: 'GET' })
+    .then(function (mapsText) {
+      console.log('Success: ', mapsText);
+      renderMaps(mapsText);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  }
+const mapDataIsValid = function(textString) {
+
+  // if (!textString) {
+  //   $("#error-msg").text("Error: The tweet contains no message.");
+  //   $("#error-msg").slideDown();
+  //   return false;
+  // }
+  //   if (textString.length > 140) {
+
+  //   $("#error-msg").text("Error: The tweet message is too long.");
+  //   $("#error-msg").slideDown();
+  //   return false;
+  // }
+  // $("#error-msg").hide();
+  // return true;
+}
+
+  $(function() {
+    let  request = null;
+    const $form = $('form');
+    //****************TODO****************************/
+    //*****this maps submit/POST needs work **********/
+    //************************************************/
+    $form.submit(function (event) {
+      event.preventDefault();
+      const mapName = $("#map_name").val();
+      console.log("mapName:",mapName);
+
+      let queryStr = $form.find("map_name, map_desc");
+      console.log("queryStr:", queryStr);
+      //****************TODO****************************/
+      //**Add data validation here**/
+
+      //****************TODO****************************/
+      //** "/maps" POST route to add a map needs to be created in routes/maps.js **/
+      request= $.ajax({ url: "/maps", method: "POST", data: queryStr});
+      // Callback handler that will be called on success
+      request.done(function (response, status, jqXHR){
+        console.log("Map POST successful.");
+        //if successful clear out the text values.
+        $("#map_name").val("");
+        $("#map_desc").val("");
+        loadMaps();
+      });
+      // Callback handler that will be called on failure
+      request.fail(function (jqXHR, status, error){
+        //if there was a failure
+        console.error("The form POST failed. Error: " + status, error);
+      });
+      // Callback handler that will be called regardless
+      // if the request failed or succeeded
+      request.always(function () {
+        //$inputs.prop("disabled", false);
+      });
+    });
+  });
+  //the map list gets rendered the first time.
+  loadMaps();
+
+});
