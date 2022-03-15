6810f91b13c420d173a4fcc1c2947cd4438eb4c6
VinceEmond <vince.emond@icloud.com>
2022-03-15 21:21:40 +0000

    Merging issues resolution

--- public/scripts/map_visuals.js
+++ public/scripts/map_visuals.js
@@ -1,6 +1,7 @@
 let map;
 
 function initMap() {
+  console.log('hit');
   let map = L.map('map').setView([49.285395314699244, -123.12673661801905], 13);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXRoYW5sb2V3ZW4iLCJhIjoiY2wwb2JhamMwMWl5bDNsbmVuczI2aXBrZyJ9.tfw9ypNjnVAzLUqlUTNZ4g', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -18,25 +19,20 @@ function initMap() {
     lng = ev.latlng.lng;
   });
 
-<<<<<<< HEAD:public/scripts/map.js
-  document.getElementById("map").addEventListener("contextmenu", function(event) {
-=======
   let marker;
   let updatePin = true;
 
-  document.getElementById("map").addEventListener("contextmenu", function (event) {
->>>>>>> 8173ba5305f1d6a09b72df0cd88816691251d61b:public/scripts/map_visuals.js
+  document.getElementById("map").addEventListener("contextmenu", function(event) {
     // Prevent the browser's context menu from appearing
     event.preventDefault();
 
-    if (testBool === false) {
+    if (updatePin === false) {
       let newLatLng = new L.LatLng(lat, lng);
       marker.setLatLng(newLatLng);
-      console.log('we in here');
+      console.log('updated...');
       return false;
     }
-    testBool = false;
-
+    updatePin = false;
 
     marker = L.marker([lat, lng]).addTo(map);
     marker.bindPopup("<img src='images/location_example.png' width='50' height='50'><br><b> Hey There! </b><br> This is a description");
@@ -44,20 +40,20 @@ function initMap() {
   });
 
   // hide all other folders
-  const hideFolders = function (selector) {
+  const hideFolders = function(selector) {
     $(selector).toggleClass('hide');
     $('.hide').slideUp();
     $(selector).toggleClass('hide');
   };
 
   // remove unwanted space bellow the current opened folder
-  const removeFolderSpace = function (selector) {
+  const removeFolderSpace = function(selector) {
     $('.folder').addClass("push-next");
     // only runs if the clicked folder is not the current open one
     if (!$(`${selector} .hide`).height() > 0) {
       $(selector).removeClass('push-next');
     }
-  }
+  };
 
   // Collapse folder sections on click
   $('#maps-collapse').click(() => {
