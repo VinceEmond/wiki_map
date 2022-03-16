6f09b8a661934ea11f7bf3f1e1a3eceb7ac95a77
Ethan Loewen <ethanloewen@gmail.com>
2022-03-16 07:03:15 +0000

    Add tags to popup html & move style to external file

--- public/scripts/map_visuals.js
+++ public/scripts/map_visuals.js
@@ -5,7 +5,7 @@ let newPinLat;
 // Place new pin on the map at lat, lng
 const generatePin = function(lat, lng, img, name, desc) {
   marker = L.marker([lat, lng]).addTo(map);
-  marker.bindPopup(`<img src='${img}' width='50' height='50'><br><b>${name}</b><br>${desc}`);
+  marker.bindPopup(`<div class='pin-popup'><img src='${img}'><h1>${name}</h1><h2>${desc}</h2></div>`);
 };
 
 // Set view and zoom for the map
@@ -48,7 +48,7 @@ function initMap() {
     updatePin = false;
 
     marker = L.marker([lat, lng]).addTo(map);
-    marker.bindPopup("<img src='images/location_example.png' width='50' height='50'><br><b> Hey There! </b><br> This is a description");
+    marker.bindPopup("<div class='pin-popup'><img src='images/location_example.png' width='50' height='50'><br><b> Hey There! </b><br> This is a description</div>");
     newPinLat = lat;
     newPinLng = lng;
     return false; // To disable default popup.
