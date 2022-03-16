da09b3d82aa48249eb80add3b6cd21d0ff87d4ab
Ethan Loewen <ethanloewen@gmail.com>
2022-03-16 18:49:57 +0000

    Final branch commit

--- public/scripts/map_points.js
+++ public/scripts/map_points.js
@@ -3,131 +3,186 @@
 /****************************/
 /*     AFTER HTML LOADS     */
 /****************************/
-$(() => {
+// $(() => {
 
 
-  /****************************/
-  /*        VARIABLES         */
-  /****************************/
-  const $newMapPoint = $('.new_pin_list form');
-  const $deleteMapPoint = $('#REPLACE-ME-WITH-A-TAG');
+/****************************/
+/*        VARIABLES         */
+/****************************/
+const $newMapPoint = $('.new_pin form');
+const $deleteMapPoint = $('#REPLACE-ME-WITH-A-TAG');
 
 
-  /****************************/
-  /*        FUNCTIONS         */
-  /****************************/
-
-  const escape = function(str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
-
-  // Create element for Single Map point
-  const createMapPointElement = function(mapPointData) {
-    const {name, description, id} = mapPointData;
-
-    const $mapPoint = $(`
-      <div class="map_point_element_wrapper">
-        <a class="name" href="/map_points/${id}">${escape(name)}</a>
-        <p>${escape(description)}</p>
-        <button type="button" id="map_point_${id}_edit"> Edit </button><button type="button" id="map_point_${id}_delete"> Delete </button>
-      </div>
-    `);
-    return $mapPoint;
-  };
-
-  // Loops through all Map_points and appends to .ejs template
-  const renderMapPointsList = function(dataObj) {
-    const mapPointsArr = dataObj.mapPoints;
-    for (let mapPoint of mapPointsArr) {
-      const $mapPoint = createMapPointElement(mapPoint);
-      $('.map_points_list').prepend($mapPoint);
-      generatePin(mapPoint.coord_x, mapPoint.coord_y, mapPoint.image, mapPoint.name, mapPoint.description);
-    }
-    return;
-  };
-
-  const loadMapPoints = function() {
-    const queryObj = {
-      // set the current map
-      map_id: currentMapId
-    };
-
-    $.ajax({url: "/map_points", method: 'GET', data: queryObj})
-      .then((mapPointsObj) => {
-        // Clears entire div of all existing map_points
-        $(".map_point_element_wrapper").remove();
-        // Render Map points with object returned from database
-        renderMapPointsList(mapPointsObj);
-      })
-      .catch(err => {
-        console.log('Load Map Points Error', err);
-      });
-  };
+/****************************/
+/*        FUNCTIONS         */
+/****************************/
 
 
-  /****************************/
-  /*      EVENT LISTENERS     */
-  /****************************/
 
+$('.map_points_list').on('click',function(event) {
+  event.preventDefault();
 
-  // Create a new map_point
-  $newMapPoint.submit(function(event) {
-    event.preventDefault();
-    console.log('newPinLat', newPinLat, 'newPinLng', newPinLng);
+  // console.log("map", map);
+  // map.removeLayer(marker);
+  // marker.clearLayer();
+  // map.remove();
+  // initMap();
+  // console.log("Map layers:", map.layers);
+  // console.log("Does it have layers?:", map.hasLayer());
+  // map.removeLayer(this);
+  // console.log("Map Point ID:",event.target.id.split('_')[2]);
+  // console.log("Edit or Delete?:",event.target.id.split('_')[3]);
 
-    const currentMapId = 1;
+  const type = event.target.id.split('_')[3];
+  const mapPointId = event.target.id.split('_')[2];
 
+  if (type === 'delete') {
     const dataForCall = {
       map_id: currentMapId,
-      owner_id: 3,
-      name: $("#ptitle").val(),
-      description: $("#pdesc").val(),
-      coord_x: 49.286328837515256,
-      coord_y: -123.12303651918343,
-      zoom: 16,
-      image: 'https://lh5.googleusercontent.com/p/AF1QipO4u7FUScRtr2QGIF9nrrbr4We-JZs9P9WixOcE=w408-h271-k-no'
+      map_point_id: mapPointId
     };
 
-    $.ajax({ url: "/map_points", method: "POST", data: dataForCall})
-      .then((response, status) =>  {
-        console.log(`Created a new map point on map_id ${currentMapId}. The new map_point's ID is: `, response.newMapPoint.id);
-        loadMapPoints();
-      })
-      .catch((err) => {
-        console.log("Error :", console.err.message);
-      });
-  });
-
-  // Delete an existing map_point
-  $deleteMapPoint.on('click',function(event) {
-    event.preventDefault();
-
-    const currentMapId = 1;
-    const currentMapPointId = 1;
-
-    const dataForCall = {
-      map_id: currentMapId,
-      map_point_id: currentMapPointId
-    };
-
-    $.ajax({ url: `map_points/${currentMapPointId}/delete`, method: "POST", data: dataForCall})
+    $.ajax({ url: `map_points/${mapPointId}/delete`, method: "POST", data: dataForCall})
       .then((response, status) => {
-        console.log(`Map ID ${currentMapId}'s map_point ${currentMapPointId} has been made inactive.`);
-        loadMapPoints();
+        console.log(`Map ID ${currentMapId}'s map_point ${mapPointId} has been made inactive.`);
+        reloadMap();
       })
       .catch((err) => {
-        console.log("Error :", err.message);
+        console.log("Error :", err);
       });
-  });
-
-
-
-  /****************************/
-  /*     ON INITIAL LOAD      */
-  /****************************/
-
-  loadMapPoints();
+  }
 
 });
+
+
+const escape = function(str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+// Create element for Single Map point
+const createMapPointElement = function(mapPointData) {
+  const {name, description, id} = mapPointData;
+
+  // <a class="name" href="/map_points/${id}">${escape(name)}</a><button type="button" id="map_point_${id}_edit"> Edit </button> <form action="/map_points/${id}/delete" method="POST"><button type="submit" id="map_point_${id}_delete"> Delete </button></form>
+  //  <a class="name" href="/map_points/${id}">${escape(name)}</a><button type="button" id="map_point_${id}_edit"> Edit </button> <form><button type="submit" id="map_point_${id}_delete"> Delete </button></form>
+  const $mapPoint = $(`
+    <div class="map_point_element_wrapper">
+
+      <a class="name" href="/map_points/${id}">${escape(name)}</a><button type="button" id="map_point_${id}_edit"> Edit </button> <form action="/map_points/${id}/delete" method="POST"><button type="submit" id="map_point_${id}_delete"> Delete </button></form>
+      <p>${escape(description)}</p>
+    </div>
+  `);
+  return $mapPoint;
+};
+
+// Loops through all Map_points and appends to .ejs template
+const renderMapPointsList = function(dataObj) {
+  const mapPointsArr = dataObj.mapPoints;
+  for (let mapPoint of mapPointsArr) {
+    const $mapPoint = createMapPointElement(mapPoint);
+    $('.map_points_list').prepend($mapPoint);
+    generatePin(mapPoint.coord_x, mapPoint.coord_y, mapPoint.image, mapPoint.name, mapPoint.description);
+  }
+  return;
+};
+
+const loadMapPoints = function() {
+  const queryObj = {
+    // set the current map
+    map_id: currentMapId
+  };
+
+  $.ajax({url: "/map_points", method: 'GET', data: queryObj})
+    .then((mapPointsObj) => {
+      // Clears entire div of all existing map_points
+      $(".map_point_element_wrapper").remove();
+      // Render Map points with object returned from database
+      renderMapPointsList(mapPointsObj);
+    })
+    .catch(err => {
+      // console.log('Load Map Points Error', err);
+    });
+};
+
+
+/****************************/
+/*      EVENT LISTENERS     */
+/****************************/
+
+$('#fav-button').on('click', function(event) {
+  event.preventDefault();
+  console.log("Fav Button clicked!");
+
+  $.ajax({ url: "/map_points/1", method: "GET"})
+    .then((response, status) => {
+      console.log("Response:", response.mapPoint);
+    })
+    .catch((err) => {
+      console.log("Error :", console.err.message);
+    });
+
+});
+
+
+// Create a new map_point
+$newMapPoint.submit(function(event) {
+  event.preventDefault();
+  // console.log('newPinLat', newPinLat, 'newPinLng', newPinLng);
+  // const currentMapId = 1;
+
+  const dataForCall = {
+    map_id: currentMapId,
+    owner_id: 3,
+    name: $("#ptitle").val(),
+    description: $("#pdesc").val(),
+    coord_x: newPinLat,
+    coord_y: newPinLng,
+    zoom: 16,
+    image: '../images/map_logo.png'
+  };
+
+  $.ajax({ url: "/map_points", method: "POST", data: dataForCall})
+    .then((response, status) =>  {
+      console.log(`Created a new map point on map_id ${currentMapId}. The new map_point's ID is: `, response.newMapPoint.id);
+      $("#ptitle").val('');
+      $("#pdesc").val('');
+      reloadMap();
+    })
+    .catch((err) => {
+      console.log("Error :", console.err.message);
+    });
+});
+
+// Delete an existing map_point
+$deleteMapPoint.on('click',function(event) {
+  event.preventDefault();
+
+  // const currentMapId = 1;
+  const currentMapPointId = 1;
+
+  const dataForCall = {
+    map_id: currentMapId,
+    map_point_id: currentMapPointId
+  };
+
+  $.ajax({ url: `map_points/${currentMapPointId}/delete`, method: "POST", data: dataForCall})
+    .then((response, status) => {
+      console.log(`Map ID ${currentMapId}'s map_point ${currentMapPointId} has been made inactive.`);
+      loadMapPoints();
+    })
+    .catch((err) => {
+      console.log("Error :", err.message);
+    });
+});
+
+
+
+/****************************/
+/*     ON INITIAL LOAD      */
+/****************************/
+
+loadMapPoints();
+
+// });
--- public/scripts/map_visuals.js
+++ public/scripts/map_visuals.js
@@ -1,11 +1,34 @@
 let map;
 let newPinLng;
 let newPinLat;
+let lat, lng;
+let marker;
+let mapLayer;
+let updatePin = true;
+
+const mapStartingLat = 49.285395314699244;
+const mapStartingLng = -123.12673661801905;
+const mapStartingZoom = 13;
 
 // Place new pin on the map at lat, lng
 const generatePin = function(lat, lng, img, name, desc) {
-  marker = L.marker([lat, lng]).addTo(map);
+  const marker = L.marker([lat, lng]);
+  marker.addTo(map);
   marker.bindPopup(`<div class='pin-popup'><img src='${img}'><h1>${name}</h1><h2>${desc}</h2></div>`);
+};
+
+const reloadMap = function() {
+  const {lat, lng} = map.getCenter();
+  const zoom =  map.getZoom();
+  updatePin = true;
+  map.remove();
+  initMap();
+  map.setView([lat, lng], zoom, {
+    zoom: {
+      animate: false
+    }
+  });
+  loadMapPoints();
 };
 
 // Set view and zoom for the map
@@ -13,124 +36,133 @@ const setView = function(lat, lng, zoom) {
   map.setView([lat, lng], zoom);
 };
 
-function initMap() {
-  map = L.map('map').setView([49.285395314699244, -123.12673661801905], 13);
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXRoYW5sb2V3ZW4iLCJhIjoiY2wwb2JhamMwMWl5bDNsbmVuczI2aXBrZyJ9.tfw9ypNjnVAzLUqlUTNZ4g', {
+
+// const initMap = function(mapViewLat, mapViewLng, zoom) {
+const initMap = function() {
+
+  // map = L.map('map').setView([mapViewLat, mapViewlng], zoom);
+  map = L.map('map').setView([mapStartingLat, mapStartingLng], mapStartingZoom);
+
+  const mapLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXRoYW5sb2V3ZW4iLCJhIjoiY2wwb2JhamMwMWl5bDNsbmVuczI2aXBrZyJ9.tfw9ypNjnVAzLUqlUTNZ4g', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZXRoYW5sb2V3ZW4iLCJhIjoiY2wwb2JhamMwMWl5bDNsbmVuczI2aXBrZyJ9.tfw9ypNjnVAzLUqlUTNZ4g'
-  }).addTo(map);
+  });
 
-  let lat, lng;
+  mapLayer.addTo(map);
+  // map.addLayer(mapLayer);
 
   map.addEventListener('mousemove', function(ev) {
     lat = ev.latlng.lat;
     lng = ev.latlng.lng;
   });
+};
 
-  let marker;
-  let updatePin = true;
 
-  document.getElementById("map").addEventListener("contextmenu", function(event) {
-    // Prevent the browser's context menu from appearing
-    event.preventDefault();
+document.getElementById("map").addEventListener("contextmenu", function(event) {
+  // Prevent the browser's context menu from appearing
+  event.preventDefault();
+  console.log("I got your pin request!");
 
-    if (updatePin === false) {
-      let newLatLng = new L.LatLng(lat, lng);
-      marker.setLatLng(newLatLng);
-      newPinLat = lat;
-      newPinLng = lng;
-      return false;
-    }
-    updatePin = false;
-
-    marker = L.marker([lat, lng]).addTo(map);
-    marker.bindPopup("<div class='pin-popup'><img src='images/location_example.png' width='50' height='50'><br><b> Hey There! </b><br> This is a description</div>");
+  if (updatePin === false) {
+    let newLatLng = new L.LatLng(lat, lng);
+    marker.bindPopup("<img src='../images/map_logo.png' width='70' height='70'><br><b> New Pin </b><br> Add a description");
+    marker.setLatLng(newLatLng);
     newPinLat = lat;
     newPinLng = lng;
-    return false; // To disable default popup.
-  });
+    return false;
+  }
+  updatePin = false;
 
-  // hide all other folders
-  const hideFolders = function(selector) {
-    $(selector).toggleClass('hide');
-    $('.hide').slideUp();
-    $(selector).toggleClass('hide');
-  };
+  marker = L.marker([lat, lng]).addTo(map);
+  marker.bindPopup("<img src='../images/map_logo.png' width='70' height='70'><br><b> New Pin </b><br> Add a description");
+  newPinLat = lat;
+  newPinLng = lng;
+  return false; // To disable default popup.
+});
 
-  // remove unwanted space bellow the current opened folder
-  const removeFolderSpace = function(selector) {
-    $('.folder').addClass("push-next");
-    // only runs if the clicked folder is not the current open one
-    if (!$(`${selector} .hide`).height() > 0) {
-      $(selector).removeClass('push-next');
-    }
-  };
+// hide all other folders
+const hideFolders = function(selector) {
+  $(selector).toggleClass('hide');
+  $('.hide').slideUp();
+  $(selector).toggleClass('hide');
+};
 
-  // Collapse folder sections on click
-  $('#maps-collapse').click(() => {
-    $('.maps_list').slideToggle();
-    hideFolders('.maps_list');
-    removeFolderSpace('.folder:nth-of-type(2)');
-  });
-  $('#pins-collapse').click(() => {
-    $('.map_points_list').slideToggle();
-    hideFolders('.map_points_list');
-    removeFolderSpace('.folder:nth-of-type(3)');
-  });
-  $('#fav-collapse').click(() => {
-    $('.favourites_list').slideToggle();
-    hideFolders('.favourites_list');
-    removeFolderSpace('.folder:nth-of-type(4)');
-  });
-  $('#contrib-collapse').click(() => {
-    $('.contributed_maps_list').slideToggle();
-    hideFolders('.contributed_maps_list');
-    removeFolderSpace('.folder:nth-of-type(5)');
-  });
-  $('#new-map-collapse').click(() => {
-    $('.new_map').slideToggle();
-    hideFolders('.new_map');
-    removeFolderSpace('.folder:nth-of-type(6)');
-  });
-  $('#new-pin-collapse').click(() => {
-    $('.new_pin').slideToggle();
-    hideFolders('.new_pin');
-    removeFolderSpace('.folder:nth-of-type(7)');
-  });
+// remove unwanted space bellow the current opened folder
+const removeFolderSpace = function(selector) {
+  $('.folder').addClass("push-next");
+  // only runs if the clicked folder is not the current open one
+  if (!$(`${selector} .hide`).height() > 0) {
+    $(selector).removeClass('push-next');
+  }
+};
 
-  // Make nav buttons collapse/expand their associated folders
-  $('#maps-button').click(() => {
-    $('.maps_list').slideToggle();
-    hideFolders('.maps_list');
-    removeFolderSpace('.folder:nth-of-type(2)');
-  });
-  $('#pins-button').click(() => {
-    $('.map_points_list').slideToggle();
-    hideFolders('.map_points_list');
-    removeFolderSpace('.folder:nth-of-type(3)');
-  });
-  $('#fav-button').click(() => {
-    $('.favourites_list').slideToggle();
-    hideFolders('.favourites_list');
-    removeFolderSpace('.folder:nth-of-type(4)');
-  });
-  $('#contrib-button').click(() => {
-    $('.contributed_maps_list').slideToggle();
-    hideFolders('.contributed_maps_list');
-    removeFolderSpace('.folder:nth-of-type(5)');
-  });
-  $('#new-map-button').click(() => {
-    $('.new_map').slideToggle();
-    hideFolders('.new_map');
-    removeFolderSpace('.folder:nth-of-type(6)');
-  });
-  $('#new-pin-button').click(() => {
-    $('.new_pin').slideToggle();
-    hideFolders('.new_pin');
-    removeFolderSpace('.folder:nth-of-type(7)');
-  });
-}
+// Collapse folder sections on click
+$('#maps-collapse').click(() => {
+  $('.maps_list').slideToggle();
+  hideFolders('.maps_list');
+  removeFolderSpace('.folder:nth-of-type(2)');
+});
+$('#pins-collapse').click(() => {
+  $('.map_points_list').slideToggle();
+  hideFolders('.map_points_list');
+  removeFolderSpace('.folder:nth-of-type(3)');
+});
+$('#fav-collapse').click(() => {
+  $('.favourites_list').slideToggle();
+  hideFolders('.favourites_list');
+  removeFolderSpace('.folder:nth-of-type(4)');
+});
+$('#contrib-collapse').click(() => {
+  $('.contributed_maps_list').slideToggle();
+  hideFolders('.contributed_maps_list');
+  removeFolderSpace('.folder:nth-of-type(5)');
+});
+$('#new-map-collapse').click(() => {
+  $('.new_map').slideToggle();
+  hideFolders('.new_map');
+  removeFolderSpace('.folder:nth-of-type(6)');
+});
+$('#new-pin-collapse').click(() => {
+  $('.new_pin').slideToggle();
+  hideFolders('.new_pin');
+  removeFolderSpace('.folder:nth-of-type(7)');
+});
+
+// Make nav buttons collapse/expand their associated folders
+$('#maps-button').click(() => {
+  $('.maps_list').slideToggle();
+  hideFolders('.maps_list');
+  removeFolderSpace('.folder:nth-of-type(2)');
+});
+$('#pins-button').click(() => {
+  $('.map_points_list').slideToggle();
+  hideFolders('.map_points_list');
+  removeFolderSpace('.folder:nth-of-type(3)');
+});
+$('#fav-button').click(() => {
+  $('.favourites_list').slideToggle();
+  hideFolders('.favourites_list');
+  removeFolderSpace('.folder:nth-of-type(4)');
+});
+$('#contrib-button').click(() => {
+  $('.contributed_maps_list').slideToggle();
+  hideFolders('.contributed_maps_list');
+  removeFolderSpace('.folder:nth-of-type(5)');
+});
+$('#new-map-button').click(() => {
+  $('.new_map').slideToggle();
+  hideFolders('.new_map');
+  removeFolderSpace('.folder:nth-of-type(6)');
+});
+$('#new-pin-button').click(() => {
+  $('.new_pin').slideToggle();
+  hideFolders('.new_pin');
+  removeFolderSpace('.folder:nth-of-type(7)');
+});
+
+
+initMap();
